f9a80892d6db849ec708b66ce951e4fda7aa0b16
logai385 <logai385@gmail.com>
2022-02-13 10:19:24 +0700

    project initial

---
+++ src/component/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+        {/* Left navbar links */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="pushmenu"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-bars" />
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="index3.html" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+        {/* Right navbar links */}
+        <ul className="navbar-nav ml-auto">
+      
+         
+          {/* Notifications Dropdown Menu */}
+   
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="fullscreen"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-expand-arrows-alt" />
+            </a>
+          </li>
+          
+        </ul>
+      </nav>
+    );
+  }
+}
